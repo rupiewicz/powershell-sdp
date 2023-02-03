9f7b6368120c58c1eb381aeca51e02469158108e
Bones <38714149+rupiewicz@users.noreply.github.com>
2023-02-03 13:15:06 +0100

    Add files via upload

---
+++ Public/Add-ServiceDeskWorklog.ps1
@@ -0,0 +1,60 @@
+﻿function Add-ServiceDeskWorklogs {
+    param (
+        # ID of the ServiceDesk Plus request
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [int[]]
+        $Id,
+
+        # Hours spent on the worklog
+        [Parameter(Mandatory)]
+        [int]
+        $Hours,
+
+        # Owner of the worklog
+        [Parameter(Mandatory)]
+        [string]
+        $Owner,
+
+        # Base URI of the ServiceDesk Plus server, i.e. https://sdp.example.com
+        [Parameter(Mandatory)]
+        $Uri,
+
+        # ServiceDesk Plus API key
+        [Parameter(Mandatory)]
+        $ApiKey
+    )
+
+    begin {
+        $InputData = @{
+            worklog = @{
+                description = $Message
+                time_spent = @{
+                    hours = $Hours
+                }
+                owner = @{
+                    name = $Owner
+                }
+            }
+        }
+    }
+
+    process {
+        foreach ($RequestId in $Id) {
+            $Parameters = @{
+                Body = @{
+                    TECHNICIAN_KEY = $ApiKey
+                    input_data = ($InputData | ConvertTo-Json -Depth 5 -Compress)
+                }
+                Method = "Post"
+                Uri = "$Uri/api/v3/requests/$RequestId/worklogs"
+            }
+
+            $Response = Invoke-RestMethod @Parameters
+
+            [PSCustomObject] @{
+                Id = $Id
+                Message = $Response.response_status.status
+            }
+        }
+    }
+}
---
+++ Public/Get-ServiceDeskRequestworklog.ps1
@@ -0,0 +1,45 @@
+﻿function Get-ServiceDeskWorklogs {
+    param (
+        # ID of the ServiceDesk Plus request
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [int[]]
+        $Id,
+
+        # Base URI of the ServiceDesk Plus server, i.e. https://sdp.example.com
+        [Parameter(Mandatory)]
+        $Uri,
+
+        # ServiceDesk Plus API key
+        [Parameter(Mandatory)]
+        $ApiKey
+    )
+
+    process {
+        foreach ($RequestId in $Id) {
+            try {
+                $Parameters = @{
+                    Body = @{
+                        TECHNICIAN_KEY = $ApiKey
+                    }
+                    Method = "Get"
+                    Uri = "$Uri/api/v3/requests/$RequestId/worklogs"
+                }
+
+                $Response = Invoke-RestMethod @Parameters
+
+                foreach ($Work in $Response.worklogs) {
+                    [PSCustomObject] @{
+                        Id = $Id
+                        WorkId = $Work.id
+                        Owner = $Work.Owner.name
+                        time_spent = $Work.time_spent.hours
+                        start_time = $Work.start_time.display_value
+                        description = $Work.description 
+                    }
+                }
+            } catch {
+                Write-Error "Error occured while getting worklogs for request with ID $RequestId. Error message: $_"
+            }
+        }
+    }
+}
